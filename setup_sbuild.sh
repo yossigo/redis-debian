f3df848dfcac7850097956c2b2ea377bfea55279
Yossi Gottlieb <yossigo@gmail.com>
2021-07-11 18:31:28 +0300

    Build and publish to Ubuntu/Debian apt repo.

---
+++ setup_sbuild.sh
@@ -0,0 +1,41 @@
+
+#!/bin/bash
+if [ $# != 1 ]; then
+    echo "Please use: setup_build.sh [dist]"
+    exit 1
+fi
+
+dist="$1"
+if ubuntu-distro-info --all | grep -Fqx "$dist"; then
+    disttype="ubuntu"
+else
+    disttype="debian"
+fi
+
+# Determine base apt repository URL based on type of distribution.
+case "$disttype" in
+    ubuntu)
+        url=http://archive.ubuntu.com/ubuntu
+        ;;
+    debian)
+        url=http://deb.debian.org/debian
+        ;;
+    *)
+        echo "Unknown distribution $disttype"
+        exit 1
+esac
+
+sbuild-createchroot \
+    --make-sbuild-tarball=/var/lib/sbuild/${dist}.tar.gz \
+    ${dist} `mktemp -d` ${url}
+
+# Ubuntu has the main and ports repositories on different URLs, so we need to
+# properly set up /etc/apt/sources.list to make cross compilation work.
+if [ "$disttype" = "ubuntu" ]; then
+    cat <<__END__ | schroot -c source:${dist}-amd64-sbuild -d / -- tee /etc/apt/sources.list
+deb [arch=amd64,i386] http://archive.ubuntu.com/ubuntu ${dist} main universe
+deb [arch=amd64,i386] http://archive.ubuntu.com/ubuntu ${dist}-updates main universe
+deb [arch=armhf,arm64] http://ports.ubuntu.com ${dist} main universe
+deb [arch=armhf,arm64] http://ports.ubuntu.com ${dist}-updates main universe
+__END__
+fi
